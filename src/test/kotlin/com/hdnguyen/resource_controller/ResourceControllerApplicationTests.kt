d82220739b65f7bb338fad415d8bf5d3d3e95b07
Hy Nguyen <hynguyen2610@gmail.com>
2024-09-09 09:32:36 +0700

    Init app and add resources controller

---
+++ src/test/kotlin/com/hdnguyen/resource_controller/ResourceControllerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hdnguyen.resource_controller
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResourceControllerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
